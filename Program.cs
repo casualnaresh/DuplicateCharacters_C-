e7f18b4944a6972fe2ad56b8be413ce956f03a2b
Naresh <casualnaresh@gmail.com>
2021-08-06 09:51:27 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DuplicateCharacters_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = DeleteDuplicates("Visual STUDIO Code");
+            Console.WriteLine(input);
+        }
+
+        static string DeleteDuplicates(string value)
+        {
+            string updatedValue = "";
+            string FinalValue = "";
+
+            foreach (char item in value.ToUpper())
+            {
+                if (updatedValue.IndexOf(item) == -1)
+                {
+                    updatedValue += item;
+                    FinalValue += item;
+                }
+            }
+            return FinalValue;
+        }
+    }
+}
